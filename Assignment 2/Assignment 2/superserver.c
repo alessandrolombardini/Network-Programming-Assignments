c53350adb4ca76e5ffadb80db262b23c679113ea
Alessandro Lombardini <alessandr.lombardin3@tudio.unibo.it>
2019-09-22 19:00:02 +0200

    Reading of file

--- Assignment 2/Assignment 2/superserver.c
+++ Assignment 2/Assignment 2/superserver.c
@@ -9,49 +9,77 @@
 #include<errno.h>
 
 //Constants and global variable declaration goes here
+#define maxNumberOfService 10
 
+int numberOfServicesLoaded;
 
 //Service structure definition goes here
-typedef struct  dataStruct {
-	char[] transportProtocol;
-	char[] serviceMode;
-	char[] servicePort;
-	char[] serviceFullPathName;
-	char[] serviceName;
+typedef struct node {
+	char transportProtocol[4];
+	char serviceMode[7];
+	char servicePort[6];
+	char serviceFullPathName[256];
+	char serviceName[256];
 	int socketFileDescriptor;
 	int pid;
-} data;
+} serviceNode;
 
-data[] dataVector;
+serviceNode services[maxNumberOfService];
 
 
 //Function prototype devoted to handle the death of the son process
 void handle_signal (int sig);
 
-int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be passed, as last argument, to execle system-call
-	// Other variables declaration goes here
-	
-	
-	// Server behavior implementation goes here
-	
-	
-	signal (SIGCHLD,handle_signal); /* Handle signals sent by son processes - call this function when it's ought to be */
-	
-	return 0;
-}
-
 // handle_signal implementation
 void handle_signal (int sig){
 	// Call to wait system-call goes here
-	
-	
+
+
 	switch (sig) {
 		case SIGCHLD : 
 			// Implementation of SIGCHLD handling goes here	
-			
-			
+
+
 			break;
 		default : printf ("Signal not known!\n");
 			break;
 	}
 }
+
+void readConfiguration() {
+	FILE *fd;
+
+	fd = fopen("inetd.conf","r");
+	if(fd==NULL){
+		printf("Si è verificato un errore in apertura del file");
+		exit(1);
+	}
+
+	int i = 0;
+	while(fscanf(fd, "%s %s %s %s\n", services[i].serviceName, services[i].transportProtocol, services[i].servicePort, services[i].serviceMode) != EOF) {
+		i++;
+	}
+	fclose(fd);
+	numberOfServicesLoaded = i;
+}
+
+void printConfiguration() {
+	for(int i = 0; i < numberOfServicesLoaded; i++){
+		printf("%s%s%s%s\n", services[i].serviceName, services[i].transportProtocol, services[i].servicePort, services[i].serviceMode);
+	}
+}
+
+int  main(int argc,char **argv,char **env){ // NOTE: env is the variable to be passed, as last argument, to execle system-call
+	// Other variables declaration goes here
+
+
+	// Server behavior implementation goes here
+	readConfiguration();
+	printConfiguration();
+
+
+	signal (SIGCHLD,handle_signal); /* Handle signals sent by son processes - call this function when it's ought to be */
+
+	return 0;
+}
+
