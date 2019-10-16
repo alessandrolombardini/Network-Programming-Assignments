bac632d92f536999575b1b22f1e16d08eae01aa4
Martina <martina.baiardi4@studio.unibo.it>
2019-10-16 17:37:27 +0200

    Ho aggiunto i file che mi hai inviato nella cartella alessandro

---
+++ Assignment 3/alessandro/tcpClient.c
@@ -0,0 +1,262 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "myfunction.h"
+
+#define MAX_BUFFER_SIZE 1024 // Maximum size of UDP messages
+#define INPUT_FILE_NAME "init.conf"
+#define STRING_SPLITTER " "
+#define PAYLOAD_CHARACTER 'a'
+#define BOOL int 
+#define TRUE 1
+#define FALSE 0
+
+BOOL checkHelloMessage(char mess[]);
+void parseHelloMessage(char mess[]);
+void sendHelloMessage();
+void sendProbeMessages();
+void sendByeMessage();
+float evaluateRTT(float rttOfProbes[]);
+
+BOOL isNumber(char * token);
+int getNumber(char * token);
+
+// Service structure definition goes here
+typedef struct node {
+	char protocolPhase[2];
+	char measureType[4];
+	int nProbes;
+	int messageSize;
+	int serverDelay;
+  int serverFD;
+} serviceNode;
+
+serviceNode service;
+
+int main(int argc, char *argv[]){
+  struct sockaddr_in server_addr; // struct containing server address information
+  struct sockaddr_in client_addr; // struct containing client address information
+  int serverFD; // Server socket filed descriptor
+  int connectResult; // Connect result
+  int stop = 0;
+  ssize_t byteRecv; // Number of bytes received
+  ssize_t byteSent; // Number of bytes sent
+  size_t msgLen; 
+  socklen_t serv_size;
+  char receivedData [MAX_BUFFER_SIZE]; // Data to be received
+  char sendData [MAX_BUFFER_SIZE]; // Data to be sent 
+
+  if (argc != 3) {
+		printf("\nErrore numero errato di parametri\n");
+		printf("\n%s <server IP (dotted notation)> <server port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+  }
+  // Open connection with the tcp server
+  serverFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (serverFD < 0){
+  	perror("(CLIENT) Socket error"); 
+  	exit(EXIT_FAILURE);
+  }
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(atoi(argv[2]));
+  server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_size = sizeof(server_addr);
+  connectResult = connect(serverFD, (struct sockaddr *) &server_addr, sizeof(server_addr));
+  if (connectResult < 0){
+    perror("(CLIENT) Connect error"); 
+    exit(EXIT_FAILURE);
+  }
+  service.serverFD = serverFD;
+
+  sendHelloMessage();
+  sendProbeMessages();
+  sendByeMessage();
+
+  return 0;
+}
+
+
+/* Send bye message to the server and receive its response */
+void sendByeMessage(){
+  char message[2]="b\n";
+  char receivedData[MAX_BUFFER_SIZE]="\0";
+  send(service.serverFD, message, strlen(message), 0);
+  printf("(CLIENT) Message sent: %s\n", message);
+  recv(service.serverFD, receivedData, MAX_BUFFER_SIZE, 0);
+  printf("(CLIENT) Message received: %s\n", receivedData);
+  if(strcmp(receivedData, "200 OK - Closing") != 0){
+    printf("(CLIENT) Error: server reject bye message\n");
+    exit(EXIT_FAILURE);
+  } 
+}
+
+/* Send probe message to the server and receive its response */
+void sendProbeMessages(){
+  char payload[MAX_BUFFER_SIZE];
+  int i;
+  
+  // Create appropriate payload
+  for(i = 0; i < service.messageSize; i++){
+    payload[i] = PAYLOAD_CHARACTER;
+  }
+  payload[i]='\0'; /* I don't know why it works */
+  // Manage probe messages
+  float rttOfProbes[service.nProbes];
+  float numberOfBits = 0;
+  for(i = 0; i < service.nProbes; i++){
+    // Create probe message
+    char completeMessage[MAX_BUFFER_SIZE] = "m ";
+    char sequenceNumber[MAX_BUFFER_SIZE];
+    sprintf(sequenceNumber, "%d ", i+1);
+    strcat(completeMessage, sequenceNumber);
+    strcat(completeMessage, payload);
+    strcat(completeMessage, "\n");
+    numberOfBits=strlen(completeMessage)*8;
+
+    // Send probe message and check its responde
+    struct timespec start, end;
+    char receivedData[MAX_BUFFER_SIZE]="\0";
+    send(service.serverFD, completeMessage, strlen(completeMessage), 0);
+    clock_gettime(CLOCK_REALTIME, &start);
+    printf("(CLIENT) Message sent: %s\n", completeMessage);
+    recv(service.serverFD, receivedData, MAX_BUFFER_SIZE, 0);
+    clock_gettime(CLOCK_REALTIME, &end);
+    rttOfProbes[i] = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000; // RTT in microseconds
+    printf("(CLIENT) Message received: %s\n", receivedData);
+    printf("(CLIENT) RTT of probe message number %i - %f milliseconds\n\n", i+1, rttOfProbes[i]/1000);
+    if(strcmp(receivedData, completeMessage) != 0){
+      printf("(CLIENT) Error: server reject probe message\n");
+      exit(EXIT_FAILURE);
+    }   
+  }
+  if(strcmp(service.measureType, "rtt") == 0){
+    printf("RTT: %f seconds\n\n", evaluateRTT(rttOfProbes)/1000);
+  } else{
+    printf("Numero di byte: %f\n", numberOfBits/8);
+    printf("Numero di kilobits: %f\n", numberOfBits/1000);
+    printf("Rtt medio in secondi: %f\n", (evaluateRTT(rttOfProbes)/1000000));
+    printf("Throughtput: %f kilobits/seconds\n\n", (numberOfBits/1000) /(evaluateRTT(rttOfProbes)/1000000));
+  }
+}
+
+float evaluateRTT(float rttOfProbes[]){
+  float total = 0;
+  for(int i = 0; i < service.nProbes; i++){
+    total += rttOfProbes[i];
+  }
+  return total/service.nProbes;
+}
+
+/* Send hello message to the server and receive its response */
+void sendHelloMessage(){
+  char receivedData [MAX_BUFFER_SIZE]; // Data to be received
+  char buffer[MAX_BUFFER_SIZE];
+  FILE * fileFD;
+
+  // Read hello message by file
+  fileFD = fopen(INPUT_FILE_NAME, "r");
+  if(fileFD == NULL){
+    perror("(CLIENT) Opening file error\n");
+    exit(EXIT_FAILURE);
+  }
+  fgets(buffer, MAX_BUFFER_SIZE, fileFD);
+  buffer[strcspn(buffer, "\n")] = ' ';
+  fclose(fileFD);
+  // Check hello message and parse it in the data structure
+  if(checkHelloMessage(buffer)){
+    parseHelloMessage(buffer);
+  }else{
+    perror("(CLIENT) Error: Hello message format isn't correct \n");
+    exit(EXIT_FAILURE);
+  }
+  strcat(buffer, "\n");
+  // Send hello message to the server and check its response
+  send(service.serverFD, buffer, strlen(buffer), 0);
+  printf("(CLIENT) Message sent: %s\n", buffer);
+  recv(service.serverFD, receivedData, MAX_BUFFER_SIZE, 0);
+  printf("(CLIENT) Message received: %s\n", receivedData);
+  if(strncmp(receivedData, "200 OK - Ready", strlen(receivedData)) != 0){
+      printf("(CLIENT) Error: server reject hello message\n");
+      exit(EXIT_FAILURE);
+  }
+}
+
+/* Extract informations by hello message */
+BOOL checkHelloMessage(char mess[]){
+    char * token;
+    char message[strlen(mess)];
+
+    strcpy(message, mess);
+    /* Check protocol phase character */
+    token = strtok(message, STRING_SPLITTER);
+    if(token == NULL || strcmp(token, "h") != 0){
+        return FALSE;
+    }
+    /* Check measure type characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || (strcmp(token, "rtt") != 0 && strcmp(token, "thput") != 0) ){
+        return FALSE;
+    }
+    /* Check number of probes characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    /* Check message size characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    /* Check server delay characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    return TRUE;
+} 
+
+/* Extract informations by hello message */
+void parseHelloMessage(char mess[]){
+    char * token;
+    char message[strlen(mess)];
+
+    strcpy(message, mess);
+    /* Check protocol phase character */
+    token = strtok(message, STRING_SPLITTER);
+    strcpy(service.protocolPhase, token);
+    /* Check measure type characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    strcpy(service.measureType, token);
+    /* Check number of probes characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    service.nProbes = getNumber(token);
+    /* Check message size characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    service.messageSize = getNumber(token);
+    /* Check server delay characters */
+    token = strtok(NULL, STRING_SPLITTER);
+    service.serverDelay = getNumber(token);
+} 
+
+/* Check if a string is a number */
+BOOL isNumber(char * token){
+    int numero = atoi(token);
+    if(numero == 0 && token[0] != '0'){
+        return FALSE;
+    }
+    return TRUE;
+}
+
+/* Get number by a string */
+int getNumber(char * token){
+    return atoi(token);
+}
+
---
+++ Assignment 3/alessandro/tcpServer.c
@@ -0,0 +1,296 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "myfunction.h"
+
+#define MAX_BUF_SIZE 1024
+#define BACK_LOG 2 // Maximum queued requests
+#define STRING_SPLITTER " "
+#define BOOL int 
+#define TRUE 1
+#define FALSE 0
+#define NO_CONNECTION_STATE 0
+#define READY_TO_REQUEST 1 
+#define ECHO_STATE 2
+#define BYE_STATE 3
+
+BOOL manageMessage(char * mess);
+BOOL checkHelloMessage(char * mess); 
+BOOL checkProbeMessage(char mess[]);
+BOOL checkByeMessage(char mess[]);
+BOOL manageHelloMessage(char * message);
+BOOL manageProbeMessage(char * message);
+BOOL manageByeMessage(char * message);
+BOOL isNumber(char * token);
+int getNumber(char * token);
+void sendMessage(char * message);
+void printService();
+void initilizeService();
+
+// Service structure definition goes here
+typedef struct node {
+	char protocolPhase[2];
+	char measureType[4];
+	int nProbes;
+	int messageSize;
+	int serverDelay;
+    int connectionFD; /* File descriptor of socket */
+    int phaseNumber; /* 0: Ready to accept connection | 
+                        1: Ready to accept service request | 
+                        2: Ready to echo back | 
+                        3: Ready response to bye */
+    int probeSequenceNumberAwaited; /* Sequence awaited: have to be in phase 2 to be usefull*/
+} serviceNode;
+
+serviceNode service;
+
+void initilizeService() {
+    /* First state of machine - other informations are invalid in NO_CONNECTION_STATE */
+    service.phaseNumber = NO_CONNECTION_STATE;
+    service.probeSequenceNumberAwaited = 1;
+}
+
+int main(int argc, char *argv[]){
+    struct sockaddr_in server_addr; // struct containing server address information
+    struct sockaddr_in client_addr; // struct containing client address information
+    int serverFD; // Server socket filed descriptor
+    int acceptFD; // Client communication socket - Accept result
+    int bindResult; // Bind result
+    int listenResult; // Listen result
+    int i;
+    int stop = 0;
+    ssize_t byteRecv; // Number of bytes received
+    ssize_t byteSent; // Number of bytes to be sent
+    socklen_t cli_size;
+    char receivedData [MAX_BUF_SIZE]; // Data to be received
+    char sendData [MAX_BUF_SIZE]; // Data to be sent
+
+    if (argc != 2) {
+            printf("\nErrore numero errato di parametri\n");
+            printf("\n%s <server port>\n", argv[0]);
+            exit(EXIT_FAILURE);
+    }
+    serverFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverFD < 0){
+        perror("socket error"); 
+        exit(EXIT_FAILURE);
+    }
+    // Initialize server address information
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[1])); 
+    server_addr.sin_addr.s_addr = INADDR_ANY; 
+    // Binding address
+    bindResult = bind(serverFD, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (bindResult < 0){
+        perror("bind error"); 
+        exit(EXIT_FAILURE);
+    }
+    cli_size = sizeof(client_addr);
+    // Listen for incoming requests
+    listenResult = listen(serverFD, BACK_LOG);
+    if (listenResult < 0){
+        perror("listen error"); 
+        exit(EXIT_FAILURE);
+    }
+    initilizeService();
+    while(TRUE){
+        // Wait for incoming requests
+        acceptFD = accept(serverFD, (struct sockaddr *) &client_addr, &cli_size);
+        if (acceptFD < 0){
+            perror("accept error"); 
+            exit(EXIT_FAILURE);
+        }
+        
+        printf("Connessione eseguita (IP: %s | Porta: %d)\n",  inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        // Accept connection and be ready to accept request of service
+        service.phaseNumber = READY_TO_REQUEST;
+        service.connectionFD = acceptFD;
+        while(service.phaseNumber != NO_CONNECTION_STATE){  // While there is communication keep connection alive
+            char data[MAX_BUF_SIZE]="\0"; /* In this way i'm sure to take only byteRecv byte */
+            byteRecv = recv(service.connectionFD, data, MAX_BUF_SIZE, 0);
+            if (byteRecv < 0){
+                perror("recv");
+                exit(EXIT_FAILURE);
+            }
+            printf("(SERVER) Message received: %s\n", data);
+            if(service.serverDelay > 0){
+                sleep(service.serverDelay);
+            }
+            if(manageMessage(data)==FALSE) {
+                initilizeService();
+            }
+        }
+        printf("Connection close\n");
+        close(acceptFD);
+        initilizeService();
+    }
+}
+
+void sendMessage(char * message) {
+    send(service.connectionFD, message, strlen(message), 0);
+    printf("(SERVER) Message sent: %s\n", message);
+}
+
+BOOL manageMessage(char mess[]){
+    BOOL check = TRUE;
+    /* Check message with message format awaited */
+    if(service.phaseNumber == READY_TO_REQUEST){
+       return manageHelloMessage(mess);
+    } else if(service.phaseNumber == ECHO_STATE){
+        return manageProbeMessage(mess);
+    } else if(service.phaseNumber == BYE_STATE) {
+        return manageByeMessage(mess);
+    }
+    return check;
+}
+
+BOOL manageHelloMessage(char * message){
+    if(checkHelloMessage(message)){
+        sendMessage("200 OK - Ready");
+        service.phaseNumber = ECHO_STATE;
+        return TRUE;
+    }else{
+        sendMessage("404 ERROR - Invalid Hello message");
+        return FALSE;
+    }
+}
+BOOL manageProbeMessage(char * message){
+    if(checkProbeMessage(message)){
+        sendMessage(message);
+        return TRUE;
+    } else {
+        sendMessage("404 ERROR - Invalid Measurement message");
+        return FALSE;
+    }
+}
+BOOL manageByeMessage(char * message){
+    if(checkByeMessage(message)){
+        sendMessage("200 OK - Closing");
+        initilizeService();
+    }else{
+        /* Don't know how to do when the message is wrong: close however. */
+        return FALSE;
+    }
+}
+
+
+BOOL checkHelloMessage(char mess[]){
+    int numero;
+    char * token;
+    char message[strlen(mess)];
+
+    strcpy(message, mess);
+    /* Check protocol phase */
+    token = strtok(message, STRING_SPLITTER);
+    if(token == NULL || strcmp(token, "h") != 0){
+        return FALSE;
+    }
+    strcpy(service.protocolPhase, token);
+    /* Check measure type */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || (strcmp(token, "rtt") != 0 && strcmp(token, "thput") != 0) ){
+        return FALSE;
+    }
+    strcpy(service.measureType, token);
+    /* Check number of probes */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    service.nProbes = getNumber(token);
+    /* Check message size */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    service.messageSize = getNumber(token);
+    /* Check server delay */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token == NULL || !isNumber(token) || getNumber(token)<0){
+        return FALSE;
+    }
+    service.serverDelay = getNumber(token);
+    return TRUE;
+} 
+
+BOOL checkProbeMessage(char mess[]){
+    char * token;
+    char message[strlen(mess)];
+
+    strcpy(message, mess);
+    /* Check protocol phase */
+    token = strtok(message, STRING_SPLITTER);
+    if(token == NULL || strcmp(token, "m") != 0){
+        return FALSE;
+    }
+    /* Check probe sequence number */
+    token = strtok(NULL, STRING_SPLITTER);
+    if(token != NULL && isNumber(token)){
+        int numero = atoi(token);
+        if(numero != service.probeSequenceNumberAwaited){
+            return FALSE;
+        }
+    } else {
+        return FALSE;
+    }
+    /* Check number of bytes of payload*/
+    int numberOfBytes = 0;
+    token = strtok(NULL, "");
+    if(token != NULL){
+        if(token[strlen(token)-1]!='\n'){
+            return FALSE;
+        }
+        numberOfBytes = strlen(token)-1;
+    }
+    if(service.messageSize != numberOfBytes){
+        return FALSE;
+    }
+    /* All is ok */
+    if(service.probeSequenceNumberAwaited == service.nProbes){
+        service.phaseNumber = BYE_STATE;
+    }
+    service.probeSequenceNumberAwaited++;
+    return TRUE;
+} 
+
+BOOL checkByeMessage(char mess[]){
+    char * token;
+    char message[strlen(mess)];
+
+    strcpy(message, mess);
+    /* Check protocol phase */
+    token = strtok(message, STRING_SPLITTER);
+    if(token == NULL || strcmp(token, "b\n") != 0){
+        return FALSE;
+    }
+    /* All is ok */
+    return TRUE;
+} 
+
+/* Check if a string is a number and if that number is greater than zero */
+BOOL isNumber(char * token){
+    int numero = atoi(token);
+    if(numero == 0 && token[0] != '0'){
+        return FALSE;
+    }
+    return TRUE;
+}
+
+/* Get number by a string */
+int getNumber(char * token){
+    return atoi(token);
+}
+
+void printService(){
+    printf("Protocol phase: %s\n",service.protocolPhase);
+    printf("Measure type: %s\n",service.measureType);
+    printf("Number of probes: %i\n",service.nProbes);
+    printf("Message size: %i\n",service.messageSize);
+    printf("Server delay: %i\n",service.serverDelay);
+}
